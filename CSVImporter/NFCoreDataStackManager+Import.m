03f083830f629fcca0d2d42595d1c0d48a20c08a
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-06-21 12:30:53 -0300

    Imported the events as well

--- CSVImporter/NFCoreDataStackManager+Import.m
+++ CSVImporter/NFCoreDataStackManager+Import.m
@@ -113,8 +113,8 @@
         igreja.cep = [self stringOrNilWithImportedString:fields[8]];
         igreja.telefones = [self stringOrNilWithImportedString:fields[9]];
         igreja.observacao = [self stringOrNilWithImportedString:fields[10]];
-        igreja.latitude = @([(NSString *)fields[11] doubleValue]);
-        igreja.longitude = @([(NSString *)fields[12] doubleValue]);
+        igreja.latitudeValue = [fields[11] doubleValue];
+        igreja.longitudeValue = [fields[12] doubleValue];
         igreja.site = [self stringOrNilWithImportedString:fields[15]];
         igreja.email = [self stringOrNilWithImportedString:fields[16]];
         igreja.lastModified = [self dateWithImportedString:fields[20]];
@@ -135,6 +135,84 @@
     return igrejas;
 }
 
+- (void)importEventsWithIgrejas:(NSDictionary *)igrejas context:(NSManagedObjectContext *)moc
+{
+    NSLog(@"Importing events...");
+
+    NSString *csvPath = [[NSBundle mainBundle] pathForResource:@"events" ofType:@"csv"];
+    NSInputStream *stream = [NSInputStream inputStreamWithFileAtPath:csvPath];
+
+    CHCSVParser *parser = [[CHCSVParser alloc] initWithInputStream:stream usedEncoding:NULL delimiter:';'];
+    parser.sanitizesFields = YES;
+
+    NFCSVParserDelegate *parserDelegate = [NFCSVParserDelegate new];
+    parser.delegate = parserDelegate;
+
+    __block int count = 0;
+
+    parserDelegate.lineBlock = ^(NSArray *fields) {
+        NSAssert(fields.count == 14, @"Expected 14 fields, got %d", fields.count);
+
+        // Skip the header
+        if ([fields[0] isEqualToString:@"id_evento"]) {
+            return;
+        }
+
+        int day = [fields[2] intValue];
+        int month = [fields[3] intValue];
+        int week = [fields[4] intValue];
+        int weekday = [fields[5] intValue];
+
+        NFEvent *event;
+
+        if (day == 0) {
+            if (week == 0) {
+                NFWeeklyEvent *weeklyEvent = [NFWeeklyEvent insertInManagedObjectContext:moc];
+                weeklyEvent.weekdayValue = weekday;
+                event = weeklyEvent;
+            } else {
+                NFMonthlyEvent *monthlyEvent = [NFMonthlyEvent insertInManagedObjectContext:moc];
+                monthlyEvent.dayValue = weekday;
+                monthlyEvent.weekValue = week;
+                event = monthlyEvent;
+            }
+        } else {
+            if (month == 0) {
+                NFMonthlyEvent *monthlyEvent = [NFMonthlyEvent insertInManagedObjectContext:moc];
+                monthlyEvent.dayValue = day;
+                event = monthlyEvent;
+            } else {
+                NFYearlyEvent *yearlyEvent = [NFYearlyEvent insertInManagedObjectContext:moc];
+                yearlyEvent.dayValue = day;
+                yearlyEvent.monthValue = month;
+                event = yearlyEvent;
+            }
+        }
+
+        event.igreja = igrejas[fields[1]];
+
+        int startHours = [fields[6] intValue];
+        int startMinutes = [fields[7] intValue];
+        event.startTimeValue = startHours * 100 + startMinutes;
+
+        int endHours = [fields[8] intValue];
+        int endMinutes = [fields[9] intValue];
+        event.endTimeValue = endHours * 100 + endMinutes;
+
+        ++count;
+    };
+
+    [parser parse];
+
+    NSLog(@"%d events imported", count);
+
+    NSError *error;
+    if (![moc save:&error]) {
+        NSLog(@"Failed to save managed object context: %@", error);
+        abort();
+    }
+}
+
 - (void)performImport
 {
     // Remove the store if it exists
@@ -150,9 +228,8 @@
     NSManagedObjectContext *moc = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
     moc.persistentStoreCoordinator = psc;
 
-    [self importIgrejasWithContext:moc];
-
-    // TODO: Import the events
+    NSDictionary *igrejas = [self importIgrejasWithContext:moc];
+    [self importEventsWithIgrejas:igrejas context:moc];
 }
 
 @end
