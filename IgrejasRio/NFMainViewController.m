f8f1bc50e2d3ac618b207313d24c4722a47b07c0
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-06-24 10:34:29 -0300

    No need to get the location ourselves

--- IgrejasRio/NFMainViewController.m
+++ IgrejasRio/NFMainViewController.m
@@ -14,8 +14,6 @@
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
-@property (strong, nonatomic) CLLocationManager *locationManager;
-
 @end
 
 @implementation NFMainViewController
@@ -28,42 +26,14 @@
 
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location, 5 * 1000, 5 * 1000);
 
+    // Point to the city center and then start tracking the user
     self.mapView.region = region;
-
-    [self focusUserLocation];
+    self.mapView.userTrackingMode = MKUserTrackingModeFollow;
 
     NSManagedObjectContext *moc = [NFCoreDataStackManager sharedManager].managedObjectContext;
     for (NFIgreja *igreja in [NFIgreja allIgrejasInContext:moc]) {
         [self.mapView addAnnotation:igreja];
     }
-}
-
-- (void)focusUserLocation
-{
-    if (self.locationManager) {
-        return;
-    }
-
-    self.locationManager = [CLLocationManager new];
-    self.locationManager.delegate = self;
-
-    [self.locationManager startUpdatingLocation];
-}
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
-{
-    CLLocation *location = [locations lastObject];
-    MKCoordinateRegion newRegion = MKCoordinateRegionMake(location.coordinate, self.mapView.region.span);
-
-    [self.mapView setRegion:newRegion animated:YES];
-
-    [self.locationManager stopUpdatingLocation];
-    self.locationManager = nil;
-}
-
-- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
-{
-    NSLog(@"Location manager error: %@", error);
 }
 
 @end
