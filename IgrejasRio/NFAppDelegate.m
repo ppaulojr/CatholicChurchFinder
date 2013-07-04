6ca9117e0b4a1408aaeca1d743f359a7940320d0
Fernando Tarlá Cardoso Lemos <fernando@netfilter.com.br>
2013-07-04 18:19:10 -0300

    Appirater integration

--- IgrejasRio/NFAppDelegate.m
+++ IgrejasRio/NFAppDelegate.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 NetFilter. All rights reserved.
 //
 
+#import <Appirater/Appirater.h>
+
 #import "NFAppDelegate.h"
 
 @implementation NFAppDelegate
@@ -17,7 +19,19 @@
     UINavigationBar *allNavBars = [UINavigationBar appearance];
     allNavBars.tintColor = [UIColor colorWithRed:53/255.0 green:139/255.0 blue:171/255.0 alpha:1];
 
+    [Appirater setAppId:@"392291029"];
+    [Appirater setDaysUntilPrompt:1];
+    [Appirater setUsesUntilPrompt:3];
+    [Appirater setSignificantEventsUntilPrompt:-1];
+    [Appirater setTimeBeforeReminding:2];
+    [Appirater appLaunched:YES];
+
     return YES;
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    [Appirater appEnteredForeground:YES];
 }
 
 @end
--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ platform :ios, '6.0'
 
 pod 'AdMob', '6.4.2'
 pod 'AFNetworking', '1.2.1'
+pod 'Appirater', '1.0.3'
 pod 'PSAlertView', '1.1'
 
 target :CSVImporter, :exclusive => true do
