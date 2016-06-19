6de4761d1c785f05d7fada89da0f8b9a13a9ecc4
Pedro Paulo Oliveira Junior <ppaulojr@gmail.com>
2016-06-19 17:18:14 -0300

    Icon

--- genIcons.sh
+++ genIcons.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+convert -resize 171x171 iTunesArtwork.png Icon@3x.png
 convert -resize 114x114 iTunesArtwork.png Icon@2x.png
 convert -resize 57x57 iTunesArtwork.png Icon.png
--- mogenerator.sh
+++ mogenerator.sh
@@ -7,6 +7,7 @@ if [ -z "$MOGENERATOR" ]; then
     MOGENERATOR=/usr/local/bin/mogenerator
     if [ ! -x "$MOGENERATOR" ]; then
         echo 'Unable to find mogenerator!' >&2
+        echo 'Install mogenerator using some package manager eg.!' >&2
         echo 'Instal it with `brew install mogenerator`' >&2
         exit 1
     fi
